6ce72ee586a886bb23992f1c909e3ae00ea382c0
CaioFelps007 <125682381+CaioFelps007@users.noreply.github.com>
2025-10-11 22:56:46 -0300

    commit 1.2

--- frontend/src/Pages/Cardapio/Cardapio.jsx
+++ frontend/src/Pages/Cardapio/Cardapio.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import "./Cardapio.css";
 
 function Cardapio() {
-  return <div> Olá</div>;
+  return (
+    <main>
+      <section className="header-cardapio">section-header</section>
+      <div className="div-cardapio">
+        <section className="sub-header">
+          <div>descrição do rebys</div>
+          <section className="scroll-options">scroll de umas parada</section>
+        </section>
+        <section className="cardapio">cardapio</section>
+      </div>
+    </main>
+  );
 }
 
 export default Cardapio;
--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -3,15 +3,31 @@ import Header from "../../components/Header/header";
 import { useNavigate } from "react-router-dom";
 import "./Home.css";
 
+//icons
+import { MdDeliveryDining } from "react-icons/md";
+import { GiPaperBagFolded } from "react-icons/gi";
+
+//images
+import acaisolto from "../../images/acaisolto.png";
+
 function Home() {
   const navigate = useNavigate();
   return (
     <div className="container-home">
+      <img src={acaisolto} className="acaisolto" />
+      <img src={acaisolto} className="acaisolto" />
+      <img src={acaisolto} className="acaisolto" />
+
       <div className="space">
         <div className="title-home">
           <h1>Reby's Açai</h1>
           <div className="slogan-home">Conheça o melhor do açaí</div>
         </div>
+        <section className="section-icons">
+          <GiPaperBagFolded />
+
+          <MdDeliveryDining />
+        </section>
 
         <button
           className="button-home"
